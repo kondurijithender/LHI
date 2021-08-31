6dd6240e9801b012269e47ed7d3acf3474af3d41
jithender.konduri <Jithender.Konduri@trigyn.com>
2021-08-31 10:14:51 +0530

    changes in progress

--- src/app/_components/configuration/configuration.component.ts
+++ src/app/_components/configuration/configuration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../_service/api.service';
+import { AlertService } from 'src/app/_service/alert.service';
 
 @Component({
   selector: 'app-configuration',
@@ -8,18 +10,36 @@ import { Component, OnInit } from '@angular/core';
 export class ConfigurationComponent implements OnInit {
 
   DimentionEdit = false;
-  
-  constructor() { }
+  industryList: any = [];
+  dimentionsList: any = [];
+  constructor(private alertService: AlertService, private _api: ApiService) { }
 
   ngOnInit(): void {
+    this.getIndustry();
+    this.getDimensions();
   }
 
   editable(): void{
     this.DimentionEdit = true;
   }
 
-  update(): void{
+  update(item: any): void{
+    console.log(item);
     this.DimentionEdit = false;
+  }
+  getDimensions() {
+    this._api.readAll("dimension").subscribe(data => {
+      this.dimentionsList = data.dimensions;
+    })
+  }
+  getIndustry() {
+    this._api.readAll("industry").subscribe(data => {
+      this.industryList = data.industries;
+    })
+  }
+  updateValue(item: any, event: any) {
+    console.log(item);
+    console.log(event.value);
   }
 
 }
