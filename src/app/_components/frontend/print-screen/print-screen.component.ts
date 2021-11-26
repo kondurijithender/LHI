20f9a1d945b18294af36e50ca85fbe9eaf4e1cbd
jithender.konduri <Jithender.Konduri@trigyn.com>
2021-11-26 14:30:58 +0530

    html to convert to pdf

--- src/app/_components/frontend/print-screen/print-screen.component.ts
+++ src/app/_components/frontend/print-screen/print-screen.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-print-screen',
@@ -6,10 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./print-screen.component.scss']
 })
 export class PrintScreenComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  downloadAsPDF(
+    timeout = 2000
+ ) {
+    const element = document.getElementById('pdfTable') as HTMLElement,
+       options = {
+          imageTimeout: timeout,
+          background: "white",
+          allowTaint: true,
+          useCORS: false,
+          height: element.clientHeight,
+          width: element.clientWidth
+       };
+
+    html2canvas(element, options).then((canvas) => {
+       let imgData = canvas.toDataURL('image/png');
+
+       let imgWidth = 210,
+          pageHeight = 250,
+          imgHeight = canvas.height * imgWidth / canvas.width,
+          heightLeft = imgHeight,
+          doc = new jsPDF('p', 'mm'),
+          position = 0;
+
+       doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+       heightLeft -= pageHeight;
+
+       while (heightLeft >= 0) {
+          position = heightLeft - imgHeight;
+          doc.addPage();
+          doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+          heightLeft -= pageHeight;
+       }
+       doc.save('test' + '.pdf');
+    });
+ }
 }
